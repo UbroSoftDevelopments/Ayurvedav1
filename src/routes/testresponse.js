848ebe85f4c14d8ad9e0e420d706dc755908ab4f
manish0pal <manish1999pal@gmail.com>
2022-12-20 11:14:45 +0530

    rank list

--- src/routes/testresponse.js
+++ src/routes/testresponse.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post("/student/", checkStudentAuth, TestResponseController.setStudentResponse);
 router.get("/student/", TestResponseController.getQuestionByStudent);
 router.get("/student/response/:testSeriesID/:paperID", checkStudentAuth, TestResponseController.getResponseOfStudent);
+router.get("/:testSeriesID/:paperID", checkAdminAuth, TestResponseController.getResultOfStudent);
 router.put("/student/startend", checkStudentAuth, TestResponseController.setTestStartEndTime);
 
 
